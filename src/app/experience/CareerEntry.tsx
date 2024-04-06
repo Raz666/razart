3b25cbf3de75e390aed87b4667aa2738a20dd6ce
Raz <gabriela.menkiewicz@gmail.com>
2024-04-06 20:48:10 +0200

    play with styling

--- src/app/experience/CareerEntry.tsx
+++ src/app/experience/CareerEntry.tsx
@@ -6,16 +6,46 @@ const CareerEntry = ({
   children: React.ReactNode;
 }) => {
   return (
-    <div className="my-6 rounded-md bg-black/80 p-4 text-emerald-50">
-      <h4 className="pb-1  text-xl font-medium tracking-wide">{company}</h4>
+    <div className="relative mb-6 mt-10 rounded-md bg-black/80 px-4 pb-4 pt-2 text-emerald-50 shadow-lg shadow-black/20">
+      <h4
+        className="absolute  -top-7 left-2 pb-1 text-xl font-extralight tracking-wide 
+      "
+      >
+        {company}
+      </h4>
 
       {children}
     </div>
   );
 };
 
+CareerEntry.Position = ({
+  position,
+  relationType = "contract",
+  locationType = "remote",
+  startDate,
+  endDate,
+}: {
+  position: string;
+  relationType?: "contract" | "fullTime";
+  locationType?: "remote" | "hybrid" | "onSite";
+  startDate: string;
+  endDate?: string;
+}) => (
+  <div className="flex justify-between">
+    <h5 className="text-md font-normal tracking-wide">{position}</h5>
+    <div>
+      <p className="pb-1 text-right text-sm font-medium text-emerald-300">
+        {`${startDate} — ${endDate ? endDate : "present"}`}
+      </p>
+      <p className="text-right text-xs font-medium text-emerald-500">
+        {relationType}, {locationType}
+      </p>
+    </div>
+  </div>
+);
 CareerEntry.Description = ({ children }: { children: React.ReactNode }) => (
-  <p className="pt-2 text-sm font-light">{children}</p>
+  <p className="py-4 text-sm font-light">{children}</p>
 );
 CareerEntry.Highlights = ({ children }: { children: React.ReactNode }) => (
   <>
@@ -35,29 +65,6 @@ CareerEntry.TitledSection = ({
   <>
     <h6 className="pb-1 pt-2 text-sm font-semibold">{title}</h6>
     <p className="text-sm font-light">{children}</p>
-  </>
-);
-CareerEntry.Position = ({
-  position,
-  relationType = "contract",
-  locationType = "remote",
-  startDate,
-  endDate,
-}: {
-  position: string;
-  relationType?: "contract" | "fullTime";
-  locationType?: "remote" | "hybrid" | "onSite";
-  startDate: string;
-  endDate?: string;
-}) => (
-  <>
-    <h5 className="text-md font-medium tracking-wide">{position}</h5>
-    <p className="py-1 text-right text-sm font-medium text-emerald-300">
-      {`${startDate} — ${endDate ? endDate : "present"}`}
-    </p>
-    <p className="text-right text-xs font-medium text-emerald-500">
-      {relationType}, {locationType}
-    </p>
   </>
 );
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${mainFont.className} antialiased`}>
-        <main className="layout mx-14 flex min-h-screen flex-col items-start bg-neutral-950/80 px-4 py-6">
-          {children}
-        </main>
+      <body
+        className={`${mainFont.className} bg-gradient-to-r from-indigo-400 from-10%  via-sky-500 via-30% to-emerald-500 to-90%  antialiased `}
+      >
+        <div className="w-100 h-100 bg-gradient-to-b from-indigo-400/30 from-10% via-sky-500/30 via-30% to-emerald-500/30 to-90%  ">
+          <div className="striped-bg w-100 h-100 bg-gradient-to-r from-emerald-500/50 from-10%  via-sky-500/50 via-70% to-indigo-500/0  ">
+            <main className="layout mx-2 flex min-h-screen flex-col items-start bg-neutral-950/80 shadow-xl shadow-black md:mx-14 lg:mx-auto lg:max-w-screen-md">
+              <div className="border-x border-black/20 px-4 py-6  ">
+                {children}
+              </div>
+            </main>
+          </div>
+        </div>
       </body>
     </html>
   );
